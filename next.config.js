69ec929c665d904829f1a50a816410e09237e272
olumi <ayind15@gmail.com>
2026-06-30 08:21:27 +0100

    fix: exclude contracts-hardhat from Next.js tsconfig build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Exclude contracts-hardhat from Next.js build
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  webpack: (config) => {
+    config.externals = config.externals || []
+    return config
+  },
 }
 
 module.exports = nextConfig
